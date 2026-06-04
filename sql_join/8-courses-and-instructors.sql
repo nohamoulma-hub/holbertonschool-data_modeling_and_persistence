91498e2567024a279de63ed836ba8d8efc4e9cb6
nohamoulma-hub <noham.oulma@gmail.com>
2026-06-04 10:57:25 +0200

    fix: change LEFT JOIN to INNER JOIN for courses and instructors

--- sql_join/8-courses-and-instructors.sql
+++ sql_join/8-courses-and-instructors.sql
@@ -1,4 +1,4 @@
 SELECT courses.title AS course_title, instructors.name
 FROM instructors
-LEFT JOIN courses ON instructors.id = courses.instructor_id
+INNER JOIN courses ON instructors.id = courses.instructor_id
 ORDER BY course_title ASC
